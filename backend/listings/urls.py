b9ce4d0b52a7655a706cbca784536c6de1169252
papycoda <opeyemi655@gmail.com>
2022-10-17 21:33:50 +0100

    remove unused directory

--- backend/listings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ListingListView, ListingDetailView, SearchListings
-
-urlpatterns = [
-    path('', ListingListView.as_view(), name='listings'),\
-    path('<slug>', ListingDetailView.as_view(), name='listing-detail'),
-    path('search', SearchListings.as_view(), name='search-listings'),
-]
